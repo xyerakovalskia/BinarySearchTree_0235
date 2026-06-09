50278a9fbdca56c753ec47cbf994dcfc27094df9
xyerakovalskia <xyera123987@gmail.com>
2026-06-09 11:12:30 +0700

    contructor untuk node class

--- searchtree.cpp
+++ searchtree.cpp
@@ -8,3 +8,12 @@ public:
     string info;
     node *leftchild;
     node *rightchild;
+
+    //constructor for node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
