500634bb911bbefb2e3ede159cbded9c7332a4ec
xyerakovalskia <xyera123987@gmail.com>
2026-06-09 11:23:30 +0700

    menambahkan fungsi if

--- searchtree.cpp
+++ searchtree.cpp
@@ -109,3 +109,17 @@ public:
 
     void postorder(node *ptr)
     {
+        if (root == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if(ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
